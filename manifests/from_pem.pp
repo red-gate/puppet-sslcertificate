fed2e12383246f0a6cac09fc0d8096a3cf46da86
Robert Chipperfield <robert.chipperfield@red-gate.com>
2019-06-12 15:56:21 +0100

    Allow importing certificates with exportable private keys

--- manifests/from_pem.pp
+++ manifests/from_pem.pp
@@ -1,8 +1,14 @@
 # Install a SSL cert using PEM format. Windows only.
-define sslcertificate::from_pem($cert_content, $key_content, $store = 'LocalMachine\My') {
+define sslcertificate::from_pem($cert_content, $key_content, $store = 'LocalMachine\My', $exportable = False) {
 
   require ::sslcertificate::openssl
 
+  if ($exportable) {
+    $exportable_flag = "-Exportable"
+  } else {
+    $exportable_flag = ""
+  }
+  
   exec { "${title}-install-to-${store}":
     provider  => powershell,
     command   => template('sslcertificate/import_from_pem.ps1.erb'),
