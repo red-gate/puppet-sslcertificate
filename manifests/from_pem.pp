a28eb695fd6dbe3b2cbe4485af82da149facd048
andrew-davison <andrew.davison@red-gate.com>
2025-10-06 15:38:39 +0100

    Update from_pem.pp

--- manifests/from_pem.pp
+++ manifests/from_pem.pp
@@ -46,13 +46,4 @@ define sslcertificate::from_pem (
       logoutput => true,
     }
   }
-
-  if $remove_expired_certs {
-    exec { "${title}_RemoveExpiredCerts":
-      provider  => 'powershell',
-      command   => template('sslcertificate/remove_expired_certs.ps1.erb'),
-      onlyif    => template('sslcertificate/should_remove_expired_certs.ps1.erb'),
-      logoutput => true,
-    }
-  }
 }
