5d294456b9f2cb959fcef14b06655059e6a1c503
andrew-davison <andrew.davison@red-gate.com>
2025-10-07 10:56:58 +0100

    tweak to allow us to not remove certs if we ever wanted to.

--- manifests/from_pem.pp
+++ manifests/from_pem.pp
@@ -20,7 +20,8 @@ define sslcertificate::from_pem (
   String $key_content,
   String $store = 'LocalMachine\My',
   Boolean $exportable = false,
-  Integer $remove_expired_certs_after = 30, # Days, default of 30, to keep the behaviour the same, option to set to 0 for immediate removal.
+  Optional[Integer] $remove_expired_certs_after = 30, # Days,
+  # Default of "30", to keep the behaviour the same, "0" for immediate removal, "undef" to not remove.
 ) {
   require sslcertificate::openssl
 
